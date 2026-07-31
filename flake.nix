742fe72f66c54bd18aadc07967641ef22ab09ed6
Pui Yong Qing <puiyongqing@gmail.com>
2026-08-01 00:01:36 +0800

    init at 0.1.0

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, self', ... }:
+        {
+          packages = {
+            lycosa = pkgs.callPackage ./package.nix { };
+            default = self'.packages.lycosa;
+          };
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ self'.packages.default ];
+            packages = with pkgs; [
+              esbuild
+              nodejs
+            ];
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              biome.enable = true;
+              deadnix.enable = true;
+              nixfmt = {
+                enable = true;
+                package = pkgs.nixfmt-rs;
+              };
+              statix.enable = true;
+            };
+          };
+        };
+
+      flake = {
+        overlays.default = final: _prev: {
+          lycosa = self.packages.${final.stdenv.hostPlatform.system}.default;
+        };
+      };
+    };
+}
---
+++ package.nix
@@ -0,0 +1,70 @@
+{
+  lib,
+  stdenv,
+  makeWrapper,
+  makeDesktopItem,
+  copyDesktopItems,
+  esbuild,
+  nwjs,
+}:
+
+stdenv.mkDerivation {
+  pname = "lycosa";
+  version = "0.1.0";
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./src/icon.png
+      ./src/package.json
+      ./src/zoom.ts
+    ];
+  };
+  nativeBuildInputs = [
+    makeWrapper
+    copyDesktopItems
+    esbuild
+  ];
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "lycosa";
+      desktopName = "Lycosa";
+      comment = "Configure the AULA HERO 84 HE keyboard";
+      exec = "lycosa";
+      icon = "lycosa";
+      categories = [ "Utility" ];
+    })
+  ];
+
+  dontConfigure = true;
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/lycosa $out/bin
+
+    cp -r src/package.json src/icon.png $out/share/lycosa/
+    esbuild src/zoom.ts --bundle --format=iife --outfile=$out/share/lycosa/zoom.js
+
+    install -Dm644 src/icon.png $out/share/icons/hicolor/512x512/apps/lycosa.png
+
+    makeWrapper ${lib.getExe nwjs} $out/bin/lycosa \
+      --add-flags "$out/share/lycosa"
+
+    runHook postInstall
+  '';
+
+  meta = {
+    mainProgram = "lycosa";
+    description = "NW.js wrapper for the AULA HERO 84 HE WebHID configuration page";
+    homepage = "https://github.com/puiyq/lycosa";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ puiyq ];
+    platforms = lib.platforms.linux;
+  };
+}
